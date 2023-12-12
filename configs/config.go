03c9b88a47c5ad07532cdbe94f7078b7020a033d
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-12 16:29:10 -0300

    feat: add custom logger

---
+++ configs/config.go
@@ -0,0 +1,20 @@
+package configs
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	return nil
+}
+
+func GetLogger(p string) *Logger {
+	// Initialize Logger
+	logger = NewLogger(p)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/augustosang/gopportunities/router"
+import (
+	"github.com/augustosang/gopportunities/configs"
+	"github.com/augustosang/gopportunities/router"
+)
+
+var (
+	logger *configs.Logger
+)
 
 func main() {
+	logger = configs.GetLogger("main")
+
+	// Initialize configs
+	err := configs.Init()
+	if err != nil {
+		logger.Errf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
