322901f8f044243f706d851a9792a31d3e3025e8
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-13 16:12:26 -0300

    feat: add request and response helpers and dtos

---
+++ dtos/openingResponseDto.go
@@ -0,0 +1,16 @@
+package dtos
+
+import "time"
+
+type OpeningResponseDto struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    *bool     `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+}
