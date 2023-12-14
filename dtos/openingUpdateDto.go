7c5397a8a8645269348d78d2a622a6cbe1edacd9
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-14 13:29:55 -0300

    feat: add update opening handler logic

---
+++ dtos/openingUpdateDto.go
@@ -0,0 +1,20 @@
+package dtos
+
+import "fmt"
+
+type UpdateOpeningDto struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningDto) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at latest one valid field must be provided")
+}
